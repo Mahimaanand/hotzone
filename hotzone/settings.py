dbd57f95ee7edd92724f25d6cbb32e65e24e7e6b
Mahimaanand <47138963+Mahimaanand@users.noreply.github.com>
2020-11-02 17:57:40 +0800

    Secret Key update

--- hotzone/settings.py
+++ hotzone/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(#ii30&q#lzi6237n*bl1$u5bccjlr)89z-ik-e7^cuh7qsk2%'
+
+SECRET_KEY = env('HOTZONE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hotzone-mahima.herokuapp.com', 'localhost', '127.0.0.1']
 
